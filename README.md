32ddb09f50b496f2dd73406a66685f1db013e3ec
Robert R. Russell <robert@rrbrussell.com>
2025-09-02 20:15:57 -0500

    Starting to reimplement the German Enigma machine.
    I have implemented:
    std::convert::Into<char> for Characters
    std::convert::Into<u8> for Characters
    std::fmt::Display for Characters

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # an_enigma_of_a_site
-A webpage that allows you to encode or decode messages compatible with the German WW2 Enigma Machines.
+A webpage that allows you to encode or decode messages compatible with the
+German WW2 Enigma Machines.
+
---
+++ enigma/src/lib.rs
@@ -0,0 +1,139 @@
+// an_enigma_of_a_site/enigma - A German WW2 Enigma machine.
+// Copyright (C) 2025 Robert R. Russell
+//
+// This program is free software: you can redistribute it and/or modify it under
+// the terms of the GNU General Public License as published by the Free Software
+// Foundation, version 3.
+//
+// This program is distributed in the hope that it will be usefull, but WITHOUT
+// ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+// FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+// details.
+//
+// You should have received a copy of the GNU General Public License along with
+// this program. If not, see <https://www.gnu.org/licenses/>.
+
+#![doc = "
+A German WW2 Enigma machine written in Rust.
+"]
+
+#[derive(Clone, Copy)]
+enum Characters {
+    A,
+    B,
+    C,
+    D,
+    E,
+    F,
+    G,
+    H,
+    I,
+    J,
+    K,
+    L,
+    M,
+    N,
+    O,
+    P,
+    Q,
+    R,
+    S,
+    T,
+    U,
+    V,
+    W,
+    X,
+    Y,
+    Z,
+}
+
+impl Characters {
+    /// Not sure if this is useful.
+    fn into_char(&self) -> char {
+        return (*self).into();
+    }
+}
+
+impl std::convert::Into<char> for Characters {
+    fn into(self) -> char {
+        match self {
+            Characters::A => 'A',
+            Characters::B => 'B',
+            Characters::C => 'C',
+            Characters::D => 'D',
+            Characters::E => 'E',
+            Characters::F => 'F',
+            Characters::G => 'G',
+            Characters::H => 'H',
+            Characters::I => 'I',
+            Characters::J => 'J',
+            Characters::K => 'K',
+            Characters::L => 'L',
+            Characters::M => 'M',
+            Characters::N => 'N',
+            Characters::O => 'O',
+            Characters::P => 'P',
+            Characters::Q => 'Q',
+            Characters::R => 'R',
+            Characters::S => 'S',
+            Characters::T => 'T',
+            Characters::U => 'U',
+            Characters::V => 'V',
+            Characters::W => 'W',
+            Characters::X => 'X',
+            Characters::Y => 'Y',
+            Characters::Z => 'Z',
+        }
+    }
+}
+
+impl std::convert::Into<u8> for Characters {
+    fn into(self) -> u8 {
+        match self {
+            Characters::A => 0,
+            Characters::B => 1,
+            Characters::C => 2,
+            Characters::D => 3,
+            Characters::E => 4,
+            Characters::F => 5,
+            Characters::G => 6,
+            Characters::H => 7,
+            Characters::I => 8,
+            Characters::J => 9,
+            Characters::K => 10,
+            Characters::L => 11,
+            Characters::M => 12,
+            Characters::N => 13,
+            Characters::O => 14,
+            Characters::P => 15,
+            Characters::Q => 16,
+            Characters::R => 17,
+            Characters::S => 18,
+            Characters::T => 19,
+            Characters::U => 20,
+            Characters::V => 21,
+            Characters::W => 22,
+            Characters::X => 23,
+            Characters::Y => 24,
+            Characters::Z => 25,
+        }
+    }
+}
+
+
+impl std::fmt::Display for Characters {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{}", <Characters as Into<char>>::into(*self))
+    }
+}
+
+/*
+impl std::ops::Add for Characters {
+    type Output = Characters;
+
+    /// This is a wrapping add.
+    fn add(self, other: Self) -> Self {
+        
+    }
+}
+*/
